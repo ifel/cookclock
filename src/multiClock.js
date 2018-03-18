f6e0401f356bcd588127f8c734d69f08117bd517
Igor Kanyuka <ifelmail@gmail.com>
2018-03-18 14:20:00 +0000

    Added countdowns on the MultiClock tab, all the clocks on that page prevent dev from sleeping

--- src/multiClock.js
+++ src/multiClock.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import Clock from './clock';
-import KeepAwake from './keepAwake';
+import Divider from 'material-ui/Divider';
 
 class MultiClock extends Component {
     render() {
         return (
             <div>
-                <Clock name='A' />
-                <Clock name='B' />
-                <Clock name='C' />
-                <Clock name='D' />
-                <Clock name='Oven' />
-                <div className="KeepAwake">
-                    <KeepAwake/>
-                </div>
+                <Clock name='A' nosleep="true" />
+                <Clock name='B' nosleep="true" />
+                <Clock name='C' nosleep="true" />
+                <Clock name='D' nosleep="true" />
+                <Clock name='Oven' nosleep="true" />
+                <Divider/>
+                <div><h3>Countdowns</h3></div>
+                <Clock countdown="true" initialTime="60" nosleep="true" />
+                <Clock countdown="true" initialTime="120" nosleep="true" />
+                <Clock countdown="true" initialTime="180" nosleep="true" />
+                <Clock countdown="true" initialTime="300" nosleep="true" />
+                <Clock countdown="true" initialTime="600" nosleep="true" />
+                <Clock countdown="true" initialTime="900" nosleep="true" />
             </div>
         );
     }
